0abb8b5e99037c60da1a137a217c1c4bf3933d8b
femunoz <felipe.munoz.dev@gmail.com>
2024-04-03 09:43:57 -0300

    uodate

--- Python/objetos/Cartesiano.py
+++ Python/objetos/Cartesiano.py
@@ -13,11 +13,18 @@ class Cartesiano:
         self.y = y
 
     # aString: Cartesiano -> str
+    # entrega un pto cartesiano en formato de str.
+    # ej: para pto1 = Cartesiano(3,4)
+    # p1.aString() entrega "(3,4)"
+    def aString(self):
+        return ( "("+str(self.x) + "," + str(self.y)+")" )
 
 
 pto1 = Cartesiano(3,4)
 pto2 = Cartesiano(5,6)
 
 # ej: quiero imprimir "(3,4)" -> "(" + "3" + "," + "4" + ")"
-print("("+str(pto1.x)+"," +str(pto1.y)+")")
+#print("("+str(pto1.x)+"," +str(pto1.y)+")")
+
+print(pto1.aString())
 
